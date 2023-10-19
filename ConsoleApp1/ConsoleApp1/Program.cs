4760f43240729c7981c9142eee7139a0eb9d8e6d
salimovrashad <selimovresad33@gmail.com>
2023-10-20 00:55:11 +0400

    add task file

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User("Rashad", "123456");
+            User[] users = {user1};
+
+            foreach (User user in users)
+            {
+                Console.WriteLine("istifadəçi hesaba daxil oldu: " + user.Username + " " + user.Password);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class User
+    {
+        public string Username;
+        public string Password;
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
